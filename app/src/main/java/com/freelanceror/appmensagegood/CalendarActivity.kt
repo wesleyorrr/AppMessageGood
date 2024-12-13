3d98383731a90fbeab7b2a0c74820a3252d34b59
Wesley de Oliveira <wesleysistemaor@gmail.com>
2024-12-13 17:50:50 -0300

    task5

--- app/src/main/java/com/freelanceror/appmensagegood/CalendarActivity.kt
+++ app/src/main/java/com/freelanceror/appmensagegood/CalendarActivity.kt
@@ -5,17 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.freelanceror.appmensagegood.R.layout.activity_calendar
+import com.freelanceror.appmensagegood.databinding.ActivityCalendarBinding
 
 class CalendarActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityCalendarBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_calendar)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+       binding = ActivityCalendarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
     }
 }
